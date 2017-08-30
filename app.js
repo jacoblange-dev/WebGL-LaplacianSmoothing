a2a017da1db49701c1d5fd035c083938a8b8e897
jake.lange <lanjak11@gmail.com>
2017-08-30 07:41:08 -0700

    fix scale

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ void main()
     gl_FragColor = vec4(0.7, 0.7, 0.7, 1.0);
 }
 `;
+
 var bunny = {};
 var InitDemo = function()
 {
@@ -112,7 +113,7 @@ var runScene = function(bunnyMesh) {
     var projMatrix = new Float32Array(16);
 
     mat4.identity(worldMatrix);
-    mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
+    mat4.lookAt(viewMatrix, [0, 0, -1], [0, 0, 0], [0, 1, 0]);
     mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
 
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
